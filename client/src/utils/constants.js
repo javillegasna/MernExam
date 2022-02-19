c411a94eefbf338946e3b0c423dc694eb30813bb
javillegasna <javillegasna@gmail.com>
2022-02-19 14:13:09 -0500

    Pre built to deploy

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_URL="http://127.0.0.1:8000/api";
+const API_URL="/api";
 //antes de cambiar debes hacer el zip para entregar sino no funcionara en local
 //hay que cambiar al terminar para deploy 
 export default API_URL;
